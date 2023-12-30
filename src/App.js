1826463cf8add17cab61e5010ddc72dd6a18ec0a
Victory Emmanuel <115421155+Victory-emmanuel@users.noreply.github.com>
2023-12-30 12:47:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import { Box, ThemeProvider, createTheme } from "@mui/material";
+import { Box } from "@mui/material";
 import { Home } from "./pages/pages";
-import { useState } from "react";
 
 function App() {
-  const [mode, setMode] = useState("dark");
-  const darkTheme = createTheme({
-    palette: {
-      mode: mode,
-    },
-  });
   return (
     <>
-      <ThemeProvider theme={darkTheme}>
-        <Box
-          bgcolor={"background.default"}
-          color={"text.primary"}
-          className="App"
-        >
-          <Home />
-        </Box>
-      </ThemeProvider>
+      <Box className="App">
+        <Home />
+      </Box>
     </>
   );
 }
